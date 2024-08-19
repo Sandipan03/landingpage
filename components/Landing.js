e2d5e35449eac5a9aab371da7a53db0b465a5472
Sandipan03 <sandipan2018@yahoo.com>
2024-08-19 13:13:39 +0530

    c6

--- components/Landing.js
+++ components/Landing.js
@@ -7,9 +7,9 @@ const Spline = React.lazy(() => import("@splinetool/react-spline"));
 gsap.registerPlugin(ScrollTrigger);
 function Landing({className}) {
   const [model, setmodel] = useState(false)
-  useEffect(() => {
-    setTimeout(()=>{setmodel(true)}, 2000)
-  }, [])
+  // useEffect(() => {
+  //   setTimeout(()=>{setmodel(true)}, 2000)
+  // }, [])
   
     useGSAP(()=>{
       gsap.timeline().to("#front",{
@@ -30,6 +30,7 @@ function Landing({className}) {
       .to("#bg",{
         // scale: 1.2,
         transformOrigin: "center center",
+        onStart:()=>setmodel(true),
         ease: "power1.inOut",
         scrollTrigger:{
           trigger:"#front",
@@ -54,37 +55,37 @@ function Landing({className}) {
           // markers: true
         }
       })
-      .from("#launch",{
-        y: 200,
-        opacity: 0,
-        duration: 0.5,
-        delay: 0.8,
-        scrollTrigger:{
-          trigger:"#front",
-          start: "top -1%",
-          end:"top -5%",
-          pin: true,
-          scrub: 3,
-          // markers: true
-        }
-      })
+      // .from("#launch",{
+      //   y: 200,
+      //   opacity: 0,
+      //   duration: 0.5,
+      //   delay: 0.8,
+      //   scrollTrigger:{
+      //     trigger:"#front",
+      //     start: "top -1%",
+      //     end:"top -5%",
+      //     pin: true,
+      //     scrub: 3,
+      //     // markers: true
+      //   }
+      // })
     })
     
-    const handleClick=()=>{
-      gsap.to("#launch",{
-        y: -200,
-        opacity: 0,
-        duration: 0.5
-      })
-    }
+    // const handleClick=()=>{
+    //   gsap.to("#launch",{
+    //     y: -200,
+    //     opacity: 0,
+    //     duration: 0.5
+    //   })
+    // }
   return (
     <div className={className} style={{ width: "100%" , overflow:"hidden"}}>
       
         {/* <img src="https://app.gemoo.com/share/image-annotation/683522250515750912?codeId=v6BO6EBXAlEQy&origin=imageurlgenerator&card=683522247906893824" alt="image" /> */}
         <img id="bg" src="./background.png" alt="" style={{position:"fixed",width:"100%",height:"100vh", overflow:"hidden"}}/>
-        <div style={{display:"flex",alignItems:"center",justifyContent:"center",position:"absolute",width:"100vw",height:"100vh"}}>
+        {/* <div style={{display:"flex",alignItems:"center",justifyContent:"center",position:"absolute",width:"100vw",height:"100vh"}}>
         <button id="launch" style={{borderRadius:"7px", zIndex:"1",marginTop:"40vh",width:"6opx",height:"40px"}} onClick={handleClick}>Let&#39;s Go</button>
-        </div>
+        </div> */}
         <Suspense fallback={<dic></dic>}>
         {model && <Spline scene='https://prod.spline.design/6Re8UFR1WXy53RFH/scene.splinecode' style={{width:"100%", height: "100vh", position:"fixed"}}/>}
         </Suspense>
