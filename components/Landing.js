9e98a757f1d7c278c7e0a501e03d064f19fe3bd2
Sandipan03 <sandipan2018@yahoo.com>
2024-08-19 12:05:59 +0530

    commit3

--- components/Landing.js
+++ components/Landing.js
@@ -1,8 +1,9 @@
-import React, {useState} from 'react'
+import React, {useState,Suspense} from 'react'
 import { gsap } from "gsap/dist/gsap";
 import { useGSAP } from '@gsap/react';
 import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import Spline from "@splinetool/react-spline";
+// import Spline from "@splinetool/react-spline";
+const Spline = React.lazy(() => import("@splinetool/react-spline"));
 gsap.registerPlugin(ScrollTrigger);
 function Landing({className}) {
   const [model, setmodel] = useState(false)
@@ -73,14 +74,16 @@ function Landing({className}) {
       })
     }
   return (
-    <div className={className} style={{overflow:"hidden"}}>
+    <div className={className} style={{ width: "100%" ,overflow:"hidden"}}>
       
         {/* <img src="https://app.gemoo.com/share/image-annotation/683522250515750912?codeId=v6BO6EBXAlEQy&origin=imageurlgenerator&card=683522247906893824" alt="image" /> */}
         <img id="bg" src="./background.png" alt="" style={{position:"fixed",width:"100%",height:"100vh", overflow:"hidden"}}/>
         <div style={{display:"flex",alignItems:"center",justifyContent:"center",position:"absolute",width:"100vw",height:"100vh"}}>
         <button id="launch" style={{borderRadius:"7px", zIndex:"1",marginTop:"40vh",width:"6opx",height:"40px"}} onClick={handleClick}>Let&#39;s Go</button>
         </div>
+        <Suspense fallback={<dic></dic>}>
         {model && <Spline scene='https://prod.spline.design/6Re8UFR1WXy53RFH/scene.splinecode' style={{width:"100%", height: "100vh", position:"fixed"}}/>}
+        </Suspense>
         <div id="frontImage">
         <img id="front" src="https://assets-global.website-files.com/63ec206c5542613e2e5aa784/643312a6bc4ac122fc4e3afa_main%20home.webp" alt="image" style={{position:"absolute",width:"100%",height:"100%", zIndex:"1"}}/>
         </div>
